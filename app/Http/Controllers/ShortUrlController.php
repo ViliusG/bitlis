bcf24e7ccb542b487df3796ad02fce22a572b17b
Vilius Grizas <viliusgrizas@gmail.com>
2024-03-08 15:53:19 +0100

    edit store response to return full url as it should be called

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ShortUrlUpsertRequest;
-use App\Models\ShortURL;
 use App\Services\ShortUrlService;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class ShortUrlController extends BaseController
@@ -13,9 +14,16 @@ class ShortUrlController extends BaseController
     public function __construct(public readonly ShortUrlService $shortUrlService)
     {}
 
-    public function store(ShortUrlUpsertRequest $request): ShortURL
+    public function store(ShortUrlUpsertRequest $request): Response
     {
-        return $this->shortUrlService->create($request->toCreateArray());
+        $shortUrl = $this->shortUrlService->create($request->toCreateArray());
+
+        return response()->json([
+            'id' => $shortUrl->id,
+            'originalUrl' => $shortUrl->original_url,
+            'shortUrl' => Str::finish(Config::get('app.url'), '/') . $shortUrl->short_url,
+            'expiryDate' => $shortUrl->expires_at
+        ]);
     }
 
     public function destroy($id): Response
